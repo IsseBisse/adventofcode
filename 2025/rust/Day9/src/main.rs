98f9a50fdc29b960f6e9da10ad696fba51e9e7c0
Isak Liljequist <isak.liljequist@cgi.com>
2025-12-10 14:27:00 +0100

    2025.9 started

---
+++ 2025/rust/Day9/src/main.rs
@@ -0,0 +1,209 @@
+use std::arch::x86_64;
+use std::collections::HashSet;
+use std::fs;
+use std::io;
+use std::cmp;
+
+use std::cmp::Ordering;
+use std::collections::BTreeSet;
+use std::collections::HashMap;
+
+fn read_input(file_path: &str) -> io::Result<Vec<String>> {
+    let contents = fs::read_to_string(file_path)?;
+    Ok(contents.lines().map(String::from).collect())
+}
+
+#[derive(Hash, Eq, PartialEq, Copy, Clone, Debug)]
+struct Point {
+    x: i64,
+    y: i64,
+}
+
+impl Point {
+    fn area(&self, other: &Self) -> i64 {
+        let dx = (self.x - other.x).abs() + 1;
+        let dy = (self.y - other.y).abs() + 1;
+        dx * dy
+    }
+
+    fn line(&self, other: &Self) -> Vec<Point> {
+        if self.x == other.x {
+            let start = cmp::min(self.y, other.y);
+            let end = cmp::max(self.y, other.y);
+            (start..end+1)
+                .map(|y| Point { x: self.x, y: y })
+                .collect::<Vec<Point>>()
+        } else {
+            let start = cmp::min(self.x, other.x);
+            let end = cmp::max(self.x, other.x);
+            (start..end+1)
+                .map(|x| Point { x: x, y: self.y })
+                .collect::<Vec<Point>>()
+        }
+    }
+
+    fn adjacent(&self) -> Vec<Point> {
+        (-1..2)
+            .flat_map(|y_offset| {
+                (-1..2)
+                    .map(move |x_offset| Point { x: self.x + x_offset, y: self.y + y_offset })
+            })
+            .filter(|p| p != self)
+            .collect::<Vec<Point>>()
+    }
+}
+
+#[derive(Debug)]
+struct PointPair {
+    p1: Point,
+    p2: Point,
+    area: i64,
+}
+
+impl PointPair {
+    fn new(p1: Point, p2: Point) -> Self {
+        let area = p1.area(&p2);
+        PointPair { p1, p2, area }
+    }
+}
+
+impl Eq for PointPair {}
+
+impl PartialEq for PointPair {
+    fn eq(&self, other: &Self) -> bool {
+        self.area == other.area
+    }
+}
+
+impl Ord for PointPair {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.area.cmp(&other.area)
+    }
+}
+
+impl PartialOrd for PointPair {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+fn parse(rows: &Vec<String>) -> Vec<Point> {
+    rows
+        .iter()
+        .map(|s| {
+            let values = s
+                .split(",")
+                .map(|sub_s| sub_s.parse::<i64>().unwrap())
+                .collect::<Vec<i64>>();
+            Point { x: values[0], y: values[1] }
+        })
+        .collect::<Vec<Point>>()
+}
+
+fn part_one() {
+    let file_path = "input.txt";
+
+    let rows = read_input(file_path).unwrap_or_else(|error| {
+        println!("Error reading file: {}", error);
+        std::process::exit(1)
+    });
+
+    let points = parse(&rows);
+
+    let mut pairs = BTreeSet::new();
+    for i in 0..points.len() {
+        for j in (i + 1)..points.len() {
+            pairs.insert(PointPair::new(points[i], points[j]));
+        }
+    }
+
+        for pair in pairs.iter().rev().take(1) {
+            println!("{:?} {:?} {}", pair.p1, pair.p2, pair.area);
+        }
+}
+
+fn print(allowed_points: &HashSet<Point>) {
+    for y in 0..15 {
+        let line: String = (0..15)
+            .map(|x| {
+                let p = Point { x: x, y: y };
+                if allowed_points.contains(&p) {
+                    '#'
+                } else {
+                    '.'
+                }
+            })
+            .collect();
+        println!("{}", line);
+    }
+}
+
+fn add_line(mut y_intervals: &HashMap<usize, HashSet<usize>>, start: Point, end: Point) {
+    if start.y == end.y {
+        y_intervals.entry(start.y as usize).or_default().insert(start.x as usize);
+        y_intervals.entry(start.y as usize).or_default().insert(end.x as usize);
+    } else {
+        for point in start.line(&end) {
+            y_intervals.entry(point.y as usize).or_default().insert(point.x as usize);
+        }
+    }
+}
+
+fn part_two() {
+    let file_path = "smallInput.txt";
+
+    let rows = read_input(file_path).unwrap_or_else(|error| {
+        println!("Error reading file: {}", error);
+        std::process::exit(1)
+    });
+
+    let points = parse(&rows);
+    let mut y_intervals: HashMap<usize, HashSet<usize>> = HashMap::new();
+    for window in points.windows(2) {
+        let start = window[0];
+        let end = window[1];
+
+        add_line(y_intervals, start, end);
+    }
+    add_line(y_intervals, points[0], points[points.len()-1]);
+
+    println!("{:?}", y_intervals);
+
+    // for point in points[0].line(&points[points.len()-1]) {
+    //     allowed_points.insert(point);
+    // }
+    
+    // print(&allowed_points);
+
+    // let mut candidates = vec![Point { x: points[0].x + 1, y: points[0].y + 1}];
+    // let mut laps = 0;
+    // while candidates.len() > 0 {
+    //     let current = candidates.pop().unwrap();
+    //     allowed_points.insert(current);
+    //     let adjacent = current.adjacent();
+
+    //     for point in adjacent {
+    //         if !allowed_points.contains(&point) {
+    //             candidates.push(point);
+    //         }
+    //     }
+
+    //     // print(&allowed_points);
+    //     // laps += 1;
+
+    //     // if laps > 3 {
+    //     //     break;
+    //     // }
+    // }
+
+    // print(&allowed_points);
+
+    // println!("{:?}", allowed_points);
+
+
+
+}
+
+fn main() {
+    // part_one();
+    part_two();
+}
